226d042129ebf81e3c61ddb9ed5ee017b7334095
hexique <69116911+hexique@users.noreply.github.com>
2025-02-24 22:57:46 +0300

    Fixed ctree command
    Fixed ctree command

--- main.py
+++ main.py
@@ -3,24 +3,26 @@
 from time import sleep, ctime
 from inspect import signature
 
-__version__ = 'a1.0.1'
-__date__ = '22.02.2025'
+__version__ = 'a1.1.1'
+__date__ = '24.02.2025'
+
+audit = ''
 
 directoryList = {
     'root': {
-        'ponos': {
-            'help.txt': '''just a help menu''', 
-            },
+        'os': {
+            'help.txt': 'just a help menu',
+            'text.txt': 'text file',
+        },
     },
 }
 
-# Глобальная переменная для хранения текущего пути
-current_path = ['root']
+current_path = []
 
 def cowsay(text):
     print(f'''
  _{"_" * len(text)}_
-| {text} |
+< {text} >
  -{"-" * len(text)}-
         \   ^__^
          \  (oo)\_______
@@ -29,45 +31,88 @@ def cowsay(text):
                 ||     ||
 ''')
 
-def cd(path):
-    global directoryList, current_path
+def goto(path):
+    global directoryList, current_path, audit
 
     parts = path.split('/')
     temp_path = current_path.copy()
 
     for part in parts:
         if part == '..':
-            if len(temp_path) > 1:
+            if len(temp_path) > 0:
                 temp_path.pop()
         elif part == '.':
             continue
         else:
-            # Получаем текущий уровень
             current_level = directoryList
             for p in temp_path:
                 current_level = current_level[p]
 
-            # Если директория не существует, создаем её
-            if part not in current_level:
-                if len(parts[-1].split('.')) != 0:
+            if '.' in part and part.split('.')[0] and part.split('.')[1]:
+                if part not in current_level:
                     current_level[part] = ''
-                    return
+                    print(f"Created file '{part}'")
+                else:
+                    while True:
+                        print(f"'{part}'")
+                        print(current_level[part])
+                        new_content = input(f"Write new value of file '{part}'\n")
+                        audit += new_content
+                        if new_content:
+                            if new_content.startswith('-w'):
+                                current_level[part] = new_content[3::]
+                            elif new_content.startswith('-a'):
+                                current_level[part] += new_content[3::]
+                            elif new_content.startswith('-d'):
+                                if input(f"Are you sure you want delete file '{part}'? (y/n)\n") == 'n':
+                                    continue
+                                else:
+                                    current_level.pop(part)
+                                    print(f"File '{part}' deleted.")
+                                    return
+                            elif new_content.startswith('-r'):
+                                current_level[part] = current_level[part][::int(new_content[3::])]
+                            elif new_content.startswith('-e'):
+                                return
+                        print(f"New value of file '{part}':\n{current_level[part]}")
+                return
+
+            if part not in current_level:
                 current_level[part] = {}
 
             temp_path.append(part)
 
-    # Обновляем текущий путь
     current_path = temp_path
-
-    print(f"{'/'.join(current_path)}", end=' ')
 
 def tree():
     print(directoryList)
 
+def ctree():
+    temp_path = directoryList
+    for i in current_path:
+        try:
+            print(f'i: {i}\ntemp_path {temp_path}\ntemp_path[i]: {temp_path[i]}\n')
+            temp_path = temp_path[i]
+        except KeyError:
+            print(f"This directory is empty.")
+            return
+    print('; '.join(list(temp_path.keys())))
+
+def comp(path):
+    temp_file = directoryList
+    for i in current_path:
+        try:
+            print(f'i: {i}\ntemp_path {temp_path}\ntemp_path[i]: {temp_path[i]}\n')
+            temp_path = temp_path[i]
+        except KeyError:
+            print(f"This directory is empty.")
+            return
 commandList = {
-    'cd': cd,
     'cowsay': cowsay,
+    'comp': comp,
+    'ctree': ctree,
     'date': lambda: print(f'Today is {ctime()}'),
+    'goto': goto,
     'tree': tree,
     'version': lambda: print(f'{__version__} {__date__}'),
 }
@@ -83,11 +128,12 @@ PPP  o  o nn  n O   O    S
 P    o  o n   n O   O    S  
 P     oo  n   n  OOO  SSS   
                             
-{__version__} {__date__}
+{__version__} {__date__} 
 '''.replace(' ', ('@', '#', '?', '/', '-', '.', ' ')[i]))
     sleep(0.1)
 
 while True:
+    print(f"{'/'.join(current_path)}", end=' ')
     command = input()
     if len(signature(commandList[command.split(' ')[0]]).parameters) == 0:
         commandList[command.split(' ')[0]]()
